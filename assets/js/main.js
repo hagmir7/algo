ee44b11f2cb5cba3860ecaa32d4cab84a678cac9
Agmir Hassane <hagmir6@gmail.com>
2023-03-07 22:34:25 +0100

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -143,10 +143,15 @@
   /**
    * Preloader
    */
-  let preloader = select('#preloader');
+  const preloader = document.querySelector('#preloader');
   if (preloader) {
     window.addEventListener('load', () => {
-      preloader.remove()
+      setTimeout(() => {
+        preloader.classList.add('loaded');
+      }, 1000);
+      setTimeout(() => {
+        preloader.remove();
+      }, 2000);
     });
   }
 
